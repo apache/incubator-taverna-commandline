1393e63baf673aea438baa4712e6274896b6f18e
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-30 15:44:23 +0000

    Tagging for Taverna 2.2.0
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.taverna-commandline/tags/taverna-commandline-1.2@11089 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna-commandline-1.2/taverna-commandline-common/src/main/java/net/sf/taverna/t2/commandline/CommandLineResultListener.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (C) 2007 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.commandline;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.taverna.t2.commandline.data.SaveResultsHandler;
+import net.sf.taverna.t2.facade.ResultListener;
+import net.sf.taverna.t2.invocation.WorkflowDataToken;
+
+import org.apache.log4j.Logger;
+
+/**
+ * A ResultListener that is using for collecting and storing results when running
+ * workflows from the commandline.
+ * 
+ * @author Stuart Owen 
+ */
+public class CommandLineResultListener implements ResultListener {
+	
+	private static final Logger logger = Logger.getLogger(CommandLineResultListener.class);
+	
+	private Map<String, WorkflowDataToken> outputMap = new HashMap<String, WorkflowDataToken>();	
+	private Map<String,WorkflowDataToken> finalTokens = new HashMap<String, WorkflowDataToken>();	
+	private final SaveResultsHandler saveResultsHandler;
+	private final int numberOfOutputs;
+	private final boolean saveIndividualResults;
+	private final boolean saveOutputDocument;
+
+	public CommandLineResultListener(int numberOfOutputs,SaveResultsHandler saveResultsHandler,boolean saveIndividualResults,boolean saveOutputDocument) {		
+		this.numberOfOutputs = numberOfOutputs;
+		this.saveResultsHandler = saveResultsHandler;
+		this.saveIndividualResults = saveIndividualResults;
+		this.saveOutputDocument = saveOutputDocument;						
+	}
+
+	public Map<String, WorkflowDataToken> getOutputMap() {
+		return outputMap;
+	}
+
+	public boolean isComplete() {		
+		return finalTokens.size() == numberOfOutputs;
+	}
+
+	public void resultTokenProduced(WorkflowDataToken token, String portName) {		
+		if (saveIndividualResults) {
+			saveResultsHandler.tokenReceived(token, portName);
+		}
+		
+		if (token.isFinal()) {
+			finalTokens.put(portName, token);		
+			if (isComplete() && saveOutputDocument) {
+				try {
+					saveResultsHandler.saveOutputDocument(finalTokens);
+				} catch (Exception e) {
+					logger.error("An error occurred saving the final results to -outputdoc",e);
+				}
+			}
+		}
+	}
+
+}
