aeea28e56080a14355caec498ae093d9b758666e
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-03-28 11:40:32 +0000

    New script that doesn't use pax runner.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-commandline/trunk@14651 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/etc/executeworkflow.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+## resolve links - $0 may be a symlink
+prog="$0"
+
+real_path() {
+    readlink -m "$1" 2>/dev/null || python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$1"
+}
+
+realprog=`real_path "$prog"`
+taverna_home=`dirname "$realprog"`
+javabin=java
+if test -x "$JAVA_HOME/bin/java"; then
+    javabin="$JAVA_HOME/bin/java"
+fi
+
+# 300 MB memory, 140 MB for classes
+exec "$javabin" -Xmx300m -XX:MaxPermSize=140m \
+  "-Dlog4j.configuration=file://$taverna_home/conf/log4j.properties " \
+  "-Djava.util.logging.config.file=$taverna_home/conf/logging.properties " \
+  "-Dtaverna.app.startup=$taverna_home" \
+  -jar "$taverna_home/bin/taverna-command-line-0.0.1-SNAPSHOT.jar" \
+  ${1+"$@"}
